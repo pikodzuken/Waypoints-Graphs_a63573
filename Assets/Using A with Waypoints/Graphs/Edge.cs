cc360568c80e6a2613ad0400e7d73fbf2b5b02c9
Pi <a63573@alunos.ipb.pt>
2026-09-23 16:58:59 +0100

    A Simple Graph API passo 1

---
+++ Assets/Using A with Waypoints/Graphs/Edge.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Edge
+{
+  public Node startNode;
+  public Node endNode;
+
+  public Edge(Node from, Node to)
+  {
+    startNode = from;
+    endNode = to;
+  }
+}
---
+++ Assets/Using A with Waypoints/Graphs/Graph.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph
+{
+    List<Edge> edge = new List<Edge>();
+    List<Node> nodes = new List<Node>();
+    List<Node> pathList = new List<Node>();
+
+    public Graph() {}
+
+    public void AddNode(GameObject id)
+    {
+        Node node = new Node(id);
+        nodes.Add(node);
+    }
+
+    public void AddEdge(GameObject fromNode, GameObject toNode)
+    {
+        Node from = FindNode(fromNode);
+        Node to = FindNode(toNode);
+
+        if(from != null && to != null)
+        {
+            Edge e = new Edge(from, to);
+            edge.Add(e);
+            from.edgeList.Add(e);
+        }
+    }
+
+    Node FindNode(GameObject id)
+    {
+        foreach (Node n in nodes)
+        {
+            if (n.getId() == id)
+                return n;
+        }
+        return null;
+    }
+}

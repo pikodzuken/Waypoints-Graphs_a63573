254eca1a8047682f282e1deba4bc0cfeabf331a3
Pi <a63573@alunos.ipb.pt>
2026-09-24 22:30:32 +0100

    A Simple Graph API passo 3

--- Assets/Using A with Waypoints/Graphs/Graph.cs
+++ Assets/Using A with Waypoints/Graphs/Graph.cs
@@ -64,7 +64,7 @@ public class Graph
             Node thisNode = open[i];
             if (thisNode.getId () == endId)
             {
-                //ReconstructPath(start,end);
+                ReconstructPath(start,end);
                 return true;
             }
 
@@ -101,6 +101,20 @@ public class Graph
             }
         }
         return false;
+    }
+
+    public void ReconstructPath(Node startId, Node endId)
+    {
+        pathList.Clear();
+        pathList.Add(endId);
+
+        var p = endId.cameFrom;
+        while (p!= startId && p != null)
+        {
+            pathList.Insert(0, p);
+            p = p.cameFrom;
+        }
+        pathList.Insert(0, startId);
     }
 
     float distance(Node a, Node b)
--- Assets/Using A with Waypoints/Graphs/Node.cs
+++ Assets/Using A with Waypoints/Graphs/Node.cs
@@ -6,9 +6,6 @@ public class Node
     public List<Edge> edgeList = new List<Edge>();
     public Node path = null;
     GameObject id;
-    public float xPos;
-    public float yPos;
-    public float zPos;
 
     public float f, g, h;
     public Node cameFrom;
@@ -16,9 +13,6 @@ public class Node
     public Node(GameObject i)
     {
         id = i;
-        xPos = i.transform.position.x;
-        yPos = i.transform.position.y;
-        zPos = i.transform.position.z;
         path = null;
     }
 
